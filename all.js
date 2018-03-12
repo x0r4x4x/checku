9017e1c1239c04de0668c3d6ead5c1c61be72367
alex <b4tm4n@gmail.com>
2018-03-13 02:40:27 +0400

    some front stuff

---
+++ all.js
@@ -0,0 +1,66 @@
+function addCell( newRow, cellText ) {
+  var newCell = newRow.insertCell(-1);
+  var newText = document.createTextNode( cellText );
+  newCell.appendChild(newText);
+}
+
+$(document).ready(function(){
+   $("button").click(function(){
+      $("#maintable tr").remove();
+      $.getJSON("summary1.php", function(result){
+           var t = document.getElementById("maintable");
+           var rI = 0;
+           var cI = 1;
+           var currentPos = '';
+           for( var key in result ) {
+              if ( result[key]._id.checkPos != currentPos )
+                {
+                    if ( currentPos == '' )
+                      {
+                         r =  t.insertRow();
+                         addCell( r, ' ' );
+                         addCell( r, "Касса " + result[key]._id.checkPos );
+                         addCell( r, ' ' );
+                         addCell( r, ' ' );
+                         r =  t.insertRow();
+                         addCell( r, result[key]._id.month );
+                         addCell( r, result[key].total );
+                         addCell( r, ' ' );
+                         addCell( r, ' ' );
+                         currentPos = result[key]._id.checkPos;
+                      }
+                    else
+                      {
+                         cI++;
+                         rI=0;
+                         c = t.rows[rI].cells;
+                         c[cI].innerHTML = "Касса " + result[key]._id.checkPos;
+                         rI++;
+                         c = t.rows[rI].cells; 
+                         c[cI].innerHTML = result[key].total;
+                         rI++;
+                         currentPos = result[key]._id.checkPos;
+                      }
+                }
+              else
+                {
+                  if ( cI == 1 )
+                    {
+                      r =  t.insertRow();
+                      addCell( r, result[key]._id.month );
+                      addCell( r, result[key].total );
+                      addCell( r, ' ' );
+                      addCell( r, ' ' );
+                    }
+                  else
+                    {
+                       c = t.rows[rI].cells;
+                       c[cI].innerHTML = result[key].total;
+                       rI++;
+
+                   }
+               }
+            }
+        });
+    });
+});
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+print "<!DOCTYPE html>
+<html>
+<head>
+<title>checku</title>
+<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>
+
+</head>
+<body>
+<button name=\"button\">Get JSON data</button><BR><BR>
+
+<script src=\"all.js\"></script>";
+
+print "<table id=\"maintable\" border=\"1\" rules=\"all\"></table>";
+print "</body></html>";
+
+?>
